73fe2f768ff3e262382487ca73675ad1a5686303
Joao Fellipe de Souza <joaofellipesouza90@gmail.com>
2026-05-14 14:22:26 -0300

    Delete ClienteDTO.java

--- ClienteDTO.java
+++
@@ -1,6 +0,0 @@
-package com.fatec.sigvs.model;
-
-public record ClienteDTO(String cpf, String nome, String cep, String endereco, String bairro, String cidade,
-        String complemento, String email) {
-
-}
